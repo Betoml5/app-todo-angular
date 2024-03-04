b3c829f1c2025df5d93f2cc335b5baa0c11b729f
Betoml5 <betoml5@hotmail.com>
2024-03-04 15:49:11 -0600

    Todo App Completada

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,35 +1,49 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Todo } from '../todo-container/todo-container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.css',
 })
 export class TodoItemComponent {
   public todoId: string = '';
   public todo: Todo | undefined;
+
   constructor(private route: ActivatedRoute) {
     this.route.params.subscribe((params) => {
-      this.todoId = params['id'];
-      console.log(params['id']);
-      console.log(this.getTodoFromLocalStorage(params['id']));
       this.todo = this.getTodoFromLocalStorage(params['id']);
     });
   }
 
   getTodoFromLocalStorage(text: string): Todo | undefined {
-    const todos = window.localStorage.getItem('todos');
-    if (todos) {
-      const parsedTodos: Todo[] = JSON.parse(todos);
-      return parsedTodos.find(
-        (todo: Todo) => todo.text.toLowerCase() === text.toLowerCase()
-      );
-    }
+    if (window.localStorage !== undefined) {
+      const todos = window.localStorage.getItem('todos');
+      if (todos) {
+        const parsedTodos: Todo[] = JSON.parse(todos);
 
+        return parsedTodos.find(
+          (todo: Todo) => todo.text.toLowerCase() === text.toLowerCase()
+        );
+      }
+    }
     return undefined;
+  }
+
+  deleteTodoFromLS() {
+    if (window.localStorage !== undefined) {
+      const todos = window.localStorage.getItem('todos');
+      if (todos) {
+        const parsedTodos: Todo[] = JSON.parse(todos);
+        const newTodos = parsedTodos.filter(
+          (todo) => todo.text.toLowerCase() !== this.todo?.text.toLowerCase()
+        );
+        window.localStorage.setItem('todos', JSON.stringify(newTodos));
+      }
+    }
   }
 }
